3ce135104b1f3bea2429dd695842cd0e6ab10174
Giuseppe Mammolo <gmammolo@gmail.com>
2021-10-30 00:24:22 +0200

    add check token

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ let app = http.createServer((req, res) => {
     // Set a response type of plain text for the response
     res.writeHead(200, {'Content-Type': 'text/plain'});
     // Send back a response and end the connection
-    res.end('Server in funzione, puoi usare tranquillamente il bot!\n');
+    if(config.telegram.token) {
+        res.end('Server in funzione, puoi usare tranquillamente il bot!\n');
+    } else {
+        res.end('Problemi con il server, verifica il log ( heroku logs --tail -a antiboomer-bot )!\n');
+    }
 });
 
 // Start the server on port 3000
@@ -24,5 +28,9 @@ app.listen(config.port, config.url);
 console.log(`Node server running on port ${config.port}`);
 
 // start bot
-const bot =  new TelegramBot(config.telegram.token);
-bot.start();
+if(config.telegram.token) {
+    const bot =  new TelegramBot(config.telegram.token);
+    bot.start();
+} else {
+    console.error('Token di telegram non trovato!')
+}
